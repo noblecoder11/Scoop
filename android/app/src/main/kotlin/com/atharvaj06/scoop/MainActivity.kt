cd818b815a356bf109c3c455bcabb6ce473e1df1
Tanmay49 <tanmaykarmarkar49@gmail.com>
2022-05-06 09:26:33 +0530

    Screenshot settings changed

--- android/app/src/main/kotlin/com/atharvaj06/scoop/MainActivity.kt
+++ android/app/src/main/kotlin/com/atharvaj06/scoop/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.atharvaj06.scoop
-
+import android.view.WindowManager.LayoutParams
 import io.flutter.embedding.android.FlutterActivity
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+}
 
 class MainActivity: FlutterActivity() {
 }
